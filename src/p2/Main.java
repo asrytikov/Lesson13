4224f65b8ae8c092fbfe59c92b1f4dda77cf444c
asrytikov <asrytikov@gmail.com>
2023-09-18 20:51:40 +0400

    Lesson13

--- src/p2/Main.java
+++ src/p2/Main.java
@@ -1,2 +1,34 @@
-package p2;public class Main {
+package p2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       Operationable operation;
+        operation = (x, y) -> x + y;
+
+        int result = operation.calculate(10,20);
+
+        System.out.println(result);
+
+        operation = new Operationable() {
+            @Override
+            public int calculate(int x, int y) {
+                return x+y;
+            }
+        };
+
+        int z = operation.calculate(20, 10);
+        System.out.println(z);
+
+        Operationable operation1 = (int x, int y)->x*y;
+        Operationable operation2 = (int x, int y)->x-y;
+
+
+    }
+
+}
+
+interface Operationable{
+    int calculate(int x, int y);
 }
